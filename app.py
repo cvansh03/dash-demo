1b703ff83a959a757d8e562bdb1550e1bea85a6a
cvansh03 <cvanshsingh03@gmail.com>
2023-12-15 01:08:44 +0530

    nya change

--- app.py
+++ app.py
@@ -1,45 +1,56 @@
-import dash
-from dash import dcc, html
-from dash.dependencies import Input, Output
 import pandas as pd
+import dash
+from dash import dcc, html, Input, Output
 import plotly.express as px
 
-# Sample Data (Replace this with your population data)
-# For this example, I'll create a simple dataset
+# Sample Data (Replace this with your actual data)
 data = {
-    'Country': ['USA', 'China', 'India', 'Brazil'],
-    'Population': [331002651, 1439323776, 1380004385, 212559417]
+    'Country': ['USA', 'China', 'Japan', 'Germany', 'India', 'UK', 'France', 'Brazil', 'Italy', 'Canada'],
+    'Population': [331002651, 1439323776, 125584838, 83783942, 1380004385, 67886011, 65273511, 212559417, 60461826, 37742154],
+    'GDP': [21427700, 14342903, 5081776, 3845630, 2875148, 2827129, 2777539, 1976948, 1849477, 1643323],
+    'PerCapitaIncome': [65212, 11218, 40285, 45699, 2099, 43173, 42658, 8717, 31952, 45085],
+    'AvgSalary': [60000, 12000, 50000, 55000, 1500, 50000, 48000, 8000, 40000, 60000]
 }
+
 df = pd.DataFrame(data)
+
+# Sorting by GDP to get top 10 countries by GDP
+df = df.nlargest(10, 'GDP')
 
 # Initialize the Dash app
 app = dash.Dash(__name__)
 
+# Dropdown options
+dropdown_options = [
+    {'label': 'Population', 'value': 'Population'},
+    {'label': 'GDP', 'value': 'GDP'},
+    {'label': 'Per Capita Income', 'value': 'PerCapitaIncome'},
+    {'label': 'Avg Salary', 'value': 'AvgSalary'}
+    # Add more options as needed
+]
+
 # Layout of the app
 app.layout = html.Div([
-    html.H1("Population Visualization"),
+    html.H1("Top 10 Countries: Different Criteria"),
     
-    # Dropdown for selecting countries
+    # Dropdown for selecting criteria
     dcc.Dropdown(
-        id='country-dropdown',
-        options=[{'label': country, 'value': country} for country in df['Country']],
-        value='USA'
+        id='criteria-dropdown',
+        options=dropdown_options,
+        value='Population'  # Default value
     ),
     
-    # Bar chart for displaying population
-    dcc.Graph(id='population-bar-chart')
+    # Bar chart for displaying selected criteria
+    dcc.Graph(id='bar-chart')
 ])
 
-# Callback to update the bar chart based on the selected country
+# Callback to update the bar chart based on the selected criteria
 @app.callback(
-    Output('population-bar-chart', 'figure'),
-    [Input('country-dropdown', 'value')]
+    Output('bar-chart', 'figure'),
+    [Input('criteria-dropdown', 'value')]
 )
-def update_bar_chart(selected_country):
-    filtered_df = df[df['Country'] == selected_country]
-    
-    fig = px.bar(filtered_df, x='Country', y='Population', title=f'Population of {selected_country}')
-    
+def update_bar_chart(selected_criteria):
+    fig = px.bar(df, x='Country', y=selected_criteria, title=f'{selected_criteria} of Top 10 Countries')
     return fig
 
 if __name__ == '__main__':
---
+++ app1.py
@@ -0,0 +1,46 @@
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output
+import pandas as pd
+import plotly.express as px
+
+# Sample Data (Replace this with your population data)
+# For this example, I'll create a simple dataset
+data = {
+    'Country': ['USA', 'China', 'India', 'Brazil'],
+    'Population': [331002651, 1439323776, 1380004385, 212559417]
+}
+df = pd.DataFrame(data)
+
+# Initialize the Dash app
+app = dash.Dash(__name__)
+
+# Layout of the app
+app.layout = html.Div([
+    html.H1("Population Visualization"),
+    
+    # Dropdown for selecting countries
+    dcc.Dropdown(
+        id='country-dropdown',
+        options=[{'label': country, 'value': country} for country in df['Country']],
+        value='USA'
+    ),
+    
+    # Bar chart for displaying population
+    dcc.Graph(id='population-bar-chart')
+])
+
+# Callback to update the bar chart based on the selected country
+@app.callback(
+    Output('population-bar-chart', 'figure'),
+    [Input('country-dropdown', 'value')]
+)
+def update_bar_chart(selected_country):
+    filtered_df = df[df['Country'] == selected_country]
+    
+    fig = px.bar(filtered_df, x='Country', y='Population', title=f'Population of {selected_country}')
+    
+    return fig
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
